e1e220093425ad5cbc79e5a3a51e511d58715319
Jerzy Matlakiewicz <jerzymatlakiewicz@gmail.com>
2023-08-14 18:24:45 +0800

    three way input added and handled

--- scenes/blocks/block.gd
+++ scenes/blocks/block.gd
@@ -3,3 +3,20 @@ extends RigidBody2D
 
 func _ready():
 	modulate = Color(randf(), randf(), randf())
+	gravity_scale = 0
+
+
+func move_left():
+	move(Vector2(-9, 0))
+
+
+func move_right():
+	move(Vector2(9, 0))
+
+
+func move_down():
+	move(Vector2(0, 9))
+
+
+func move(vector: Vector2):
+	global_position += vector
--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,10 +1,12 @@
 extends Node
 
 @onready var spawn_position = $SpawnPosition
+@onready var move_timer = $MoveTimer
 
 @export var blocks: Array[PackedScene]
 
 var current_block = null
+var can_move = true
 
 
 func _ready():
@@ -12,10 +14,32 @@ func _ready():
 	for i in 5:
 		current_block = spawn_block()
 		await current_block.body_entered
+		current_block.gravity_scale = 1
+
+
+func _process(_delta):
+	if not can_move:
+		return
+	
+	if Input.is_action_pressed("left"):
+		current_block.move_left()
+	elif Input.is_action_pressed("right"):
+		current_block.move_right()
+	elif Input.is_action_pressed("down"):
+		current_block.move_down()
+	
+	await_move_timer()
+
+
+func await_move_timer():
+	can_move = false
+	move_timer.start()
+	await move_timer.timeout
+	can_move = true
 
 
 func spawn_block():
-	var block = blocks.pick_random().instantiate()
+	var block = blocks.pick_random().instantiate() as RigidBody2D
 	call_deferred("add_child", block)
 	block.global_position = spawn_position.global_position
 	return block
