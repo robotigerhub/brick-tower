352257d18d83b3f2c2b5b5cec9186628dbcf7f08
Jerzy Matlakiewicz <jerzymatlakiewicz@gmail.com>
2023-08-22 22:01:06 +0800

    random support creation system introduced

--- scenes/controllers/support_controller.gd
+++ scenes/controllers/support_controller.gd
@@ -1,1 +1,21 @@
 extends Node
+
+@export var support_scene: PackedScene
+
+var positions = {
+	0: {
+		"x": [500, 518, 536, 554, 572, 590],
+		"y": [500, 518, 536, 554, 572, 590]
+	}
+}
+
+
+func _ready():
+	for i in 8:
+		spawn_support()
+
+
+func spawn_support():
+	var support = support_scene.instantiate()
+	add_child(support)
+	support.global_position = Vector2(positions[0]["x"].pick_random(), positions[0]["y"].pick_random())
